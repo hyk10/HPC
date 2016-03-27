ed346bf4b655e1f4289ee0535d9b42cb3919ba92
hyk10 <hong.kim10@imperial.ac.uk>
2016-03-27 11:45:11 +0100

    Debugging Q3 complete

--- Q3/main.cpp
+++ Q3/main.cpp
@@ -12,25 +12,25 @@ using namespace std;
 
 int main()
 {
-    double L=1.0;
-    int N_x=20;
-    double T=5.0;
-    double N_t=5000.0;
-    double alpha=1.0;
-    double theta=0.5;
+    double L;
+    int N_x;
+    double T;
+    double N_t;
+    double alpha;
+    double theta;
 
-    //cout << "Enter L value in double format(eg 1.0) :" << endl;
-    //cin >> L;
-    //cout << "Enter number of discretised domain in integer format (eg 20) :" << endl;
-    //cin >> N_x;
-    //cout << "Enter target time T in double format (eg 5.0) :" << endl;
-    //cin >> T;
-    //cout << "Enter number of time step in double format (eg 5000.0) :" << endl;
-    //cin >> N_t;
-    //cout << "Enter alpha value in double format (eg 1.0):" << endl;
-    //cin >> alpha;
-    //cout << "Enter theta value(rad) in double format (eg 0.5):" << endl;
-    //cin >> theta;
+    cout << "Enter L value in double format(eg 1.0) :" << endl;
+    cin >> L;
+    cout << "Enter number of discretised domain in integer format (eg 20) :" << endl;
+    cin >> N_x;
+    cout << "Enter target time T in double format (eg 5.0) :" << endl;
+    cin >> T;
+    cout << "Enter number of time step in double format (eg 5000.0) :" << endl;
+    cin >> N_t;
+    cout << "Enter alpha value in double format (eg 1.0):" << endl;
+    cin >> alpha;
+    cout << "Enter theta value(rad) in double format (eg 0.5):" << endl;
+    cin >> theta;
 
     double gamma_0 = 0.0;
     double gamma_1 = 0.0;
@@ -58,7 +58,7 @@ int main()
 
         IpnuL.matrixMultiplication(vU_heat, gamma_0, gamma_1);
         refT+= d_t;
-        cout << refT << endl;
+
     };
 
 
@@ -66,8 +66,6 @@ int main()
     for (unsigned int i = 0; i < vU_heat.size(); i++)
     {
         deviation += (vU_heat[i]-sin(pi*i*d_x/L)*exp(-alpha*pi*pi*refT/(L*L)))*(vU_heat[i]-sin(pi*i*d_x/L)*exp(-alpha*pi*pi*refT/(L*L)));
-
-        //cout << sin(pi*i*d_x/L)*exp(-alpha*pi*pi*refT/(L*L)) << endl;
     };
 
     rmse = sqrt(deviation/(N_x+1));
